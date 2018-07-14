5862ea0bcecd3bb146585c8186fec59efb6d8e0b
李珍 <lizhen@lizhendeMacBook-Pro.local>
2018-07-14 15:18:27 +0800

    change3

--- OPMS/src/com/opms/entity/NeedSearch.java
+++ OPMS/src/com/opms/entity/NeedSearch.java
@@ -5,14 +5,7 @@ public class NeedSearch {
 	private Integer stage;
 	private Long acceptid;
 	private String keyword;
-	private Long projectid;
 	
-	public Long getProjectid() {
-		return projectid;
-	}
-	public void setProjectid(Long projectid) {
-		this.projectid = projectid;
-	}
 	public Integer getStage() {
 		return stage;
 	}
