8c649107ca08eb9ba0c90a90d564564824c8c5c7
lizhenzhen9999 <30432413+lizhenzhen9999@users.noreply.github.com>
2018-07-16 16:13:27 +0800

    Update NeedSearch.java

--- OPMS/src/com/opms/entity/NeedSearch.java
+++ OPMS/src/com/opms/entity/NeedSearch.java
@@ -4,12 +4,7 @@ public class NeedSearch {
 
 	private Integer stage;
 	private Long acceptid;
-	public Long getProject(){
-	      return project;
-	}
-  	public void setProject(Long project ){
-	      this.project=project;
-	}	
+		
 	public Integer getStage() {
 		return stage;
 	}
@@ -19,24 +14,13 @@ public class NeedSearch {
 	public Long getAcceptid() {
 		return acceptid;
 	}
-	public void setProject(String project){
-		this.project=project
-	}
-	public String getProject(){
-	  return project;	
-	}
 	public void setAcceptid(Long acceptid) {
 		this.acceptid = acceptid;
 	}
-	public String getKeyword() {
-		return keyword;
-	}
-	public void setKeyword(String keyword) {
-		this.keyword = keyword;
-	}
+	
 	@Override
 	public String toString() {
 		return "NeedSearch [status=" + status + ", stage=" + stage
-				+ ", acceptid=" + acceptid + ", keywords=" + keywords + "]";
+				+ ", acceptid=" + acceptid +"]";
 	}
 }
